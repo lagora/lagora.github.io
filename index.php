c518292c2dee271723bf02df4de0f92567384642
François Lunette <flunette@netmessage.com>
2014-04-15 15:53:32 +0200

    layout

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-	echo "Hello World ! 2" ;
-?>
+<!DOCTYPE html>
+<html class="<?php echo $agent_datas;?>">
+	<head>
+		<meta charset="utf-8">
+		<title><?php if (isset($title))	echo $title; ?></title>
+	  <meta name="description" content="">
+	  <meta name="author" content="">
+	  <meta name="viewport" content="
+	          height = device-height,
+	          width = device-width,
+	          initial-scale = 1.0,
+	          minimum-scale = 1.0,
+	          maximum-scale = 1.0,
+	          user-scalable = no">
+	  <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js" type="text/javascript" charset="utf-8"></script>
+	</head>
+	<body>
+		<h1>Nope.</h1>
+	</body>
+</html>
