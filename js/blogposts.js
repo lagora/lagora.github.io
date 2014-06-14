599018d7effe57d39d120f59ae1f6ada84daf805
lagora <lagora.franck@gmail.com>
2014-06-14 13:12:06 +0200

    fixes ?

--- js/blogposts.js
+++ js/blogposts.js
@@ -20,7 +20,7 @@ function load_article (y, m, d, target)
 			if (1 == d) {
 				$('body').data('until-month', m-1);
 			}
-			$(target).parent().append('<li class="blog article" date="'+date+'">'+y+'/'+m+'/'+d+'<br/>'+datas+'</li>');
+			$(target.parent()).append('<li class="blog article" date="'+date+'">'+y+'/'+m+'/'+d+'<br/>'+datas+'</li>');
 			$('body').data('loaded', $('body').data('loaded')+1);
 			loaded = true;
 		});
