5425ee46a3b8ceb9777395b906826d9fd89d70c4
Franck Lagora <lagora.franck@gmail.com>
2015-01-19 17:40:27 +0100

    Update test_02.js

--- js/test_02.js
+++ js/test_02.js
@@ -66,7 +66,7 @@ var items = [
 ];
 $(document).ready(function () {
 	console.log(items);
-	var itemTemplate = '<div class="item"><a href="{{url}}" target="_blank"><i class="{{icon}} fa-3x"></i>&nbsp;{{name}}</a></div>';
+	var itemTemplate = '<div class="item"><a href="{{url}}" target="_blank"><i class="{{icon}} fa-3x"></i>{{name}}</a></div>';
 	items.forEach(function (item, index) {
 		console.log('\n');
 		var clonedTemplate = itemTemplate;
