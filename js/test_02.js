989fff6d4de5b3e48653c361b218ae840e7d4525
Franck Lagora <lagora.franck@gmail.com>
2015-01-20 19:13:18 +0100

    Update test_02.js

--- js/test_02.js
+++ js/test_02.js
@@ -94,7 +94,7 @@ $(document).ready(function () {
 	$('.item.game').click(function (e) {
 		e.preventDefault();
 		var target = e.currentTarget;
-		var url = $(e).attr('url');
+		var url = $(e).attr('href')+'.md';
 		console.log(target, url);
 		$.get({
 			url: url, 
