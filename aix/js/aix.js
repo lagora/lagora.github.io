c08406c85fbc32f2e556be568614d63fbef53fc3
Franck Lagora <lagora.franck@gmail.com>
2015-03-05 14:14:17 +0100

    Update aix.js

--- aix/js/aix.js
+++ aix/js/aix.js
@@ -7,7 +7,7 @@ var init = function () {
     ],
       menu,
       main;
-  console.log(links);
+
   document.getElementById('links').innerHTML = '<ul class="links-list"></ul>';
   links
   .filter(function (link) {
