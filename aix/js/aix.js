e48f965868143100977ec16c842b8c696b4d13e8
Franck Lagora <lagora.franck@gmail.com>
2015-03-05 14:43:00 +0100

    Update aix.js

--- aix/js/aix.js
+++ aix/js/aix.js
@@ -12,15 +12,14 @@ var init = function () {
       menu,
       main;
 
-  document.getElementById('links').innerHTML = '<ul class="links-list"></ul>';
-  links
-  .filter(function (link) {
-    return !!link.url;
-  })
-  .map(function (link) {
-    document.querySelector('#links ul').innerHTML += '<li class="link-item"><a href="' + link.url + '">' + link.text + '</a><li>';
-  });    
+  //document.getElementById('links').innerHTML = '<ul class="links-list"></ul>';
+  var tmp = links.filter(function (link) {return !!link.url;}).map(function (link) {
+     return '<li class="link-item"><a href="#' + link.url + '">' + link.text + '</a><li>';
+  }).join('</li><li>');
   
+  if (9 < tmp.length) {
+    document.getElementById('links').innerHTML = '<ul class="links-list">' + tmp + '</ul>';
+  }
 };
 
 if ('function' === typeof document.addEventListener) {
