b63b1bad36c118a3da9925ddefc4428abb8b8781
Franck Lagora <lagora.franck@gmail.com>
2015-05-16 22:58:05 +0200

    Update sonofapix.js

--- sonofapix/sonofapix.js
+++ sonofapix/sonofapix.js
@@ -1,14 +1,14 @@
 var sonofapix = {
-  "head": {
-    "link": [
-      {"type": "text/css","rel": "stylesheet","href": "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"},
-      {"type": "text/css","rel": "stylesheet","href": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css"}
-    ],
-    "script": [
-      {"type": "text/javascript", "src": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"},
-      {"type": "text/javascript", "src": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"}
-    ]
-  },
+  // "head": {
+  //   "link": [
+  //     {"type": "text/css","rel": "stylesheet","href": "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"},
+  //     {"type": "text/css","rel": "stylesheet","href": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css"}
+  //   ],
+  //   "script": [
+  //     {"type": "text/javascript", "src": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"},
+  //     {"type": "text/javascript", "src": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"}
+  //   ]
+  // },
   "links": {
     "mail": {"icon": "fa fa-envelope", "href": "mailto:lagora@sonofapix.com"},
     "twitter": {"icon": "fa fa-twitter", "href": "http://www.twitter.com/sonofapix"},
@@ -37,12 +37,12 @@ var sonofapix = {
   }
 };
 
-Object.keys(sonofapix.head).map(function (head_element) {
-  sonofapix.head[head_element].map(function (each_element) {
-    var element = document.createElement(head_element);
-    Object.keys(each_element).map(function (element_attribute) {
-      element[element_attribute] = each_element[element_attribute];
-    });
-    document.head.appendChild(element);
-  });
-});
+// Object.keys(sonofapix.head).map(function (head_element) {
+//   sonofapix.head[head_element].map(function (each_element) {
+//     var element = document.createElement(head_element);
+//     Object.keys(each_element).map(function (element_attribute) {
+//       element[element_attribute] = each_element[element_attribute];
+//     });
+//     document.head.appendChild(element);
+//   });
+// });
