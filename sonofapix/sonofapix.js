9e66156635bebebd59c4f2151c4bf85ce2d29106
Franck Lagora <lagora.franck@gmail.com>
2015-05-16 21:59:49 +0200

    Update sonofapix.js

--- sonofapix/sonofapix.js
+++ sonofapix/sonofapix.js
@@ -1,3 +1,20 @@
+/*
+var css_bootstrap = document.createElement('link');
+css_bootstrap.type = "text/javascript";
+css_bootstrap.rel = "stylesheet";
+css_bootstrap.href = "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css";
+document.head.appendChild(css_bootstrap);
+
+var script_jquery = document.createElement('script');
+script_jquery.type = "text/javascript";
+script_jquery.src = "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.head.appendChild(script_jquery);
+
+var script_jquery = document.createElement('script');
+script_jquery.type = "text/javascript";
+script_jquery.src = "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js";
+document.head.appendChild(script_jquery);
+*/
 $(document).ready(function () {
   
 });
