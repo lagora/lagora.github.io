c85fdbc5860dabfe58e7a5c3a48c580ee38fa13a
Franck Lagora <lagora.franck@gmail.com>
2015-05-16 22:32:27 +0200

    Update sonofapix.js

--- sonofapix/sonofapix.js
+++ sonofapix/sonofapix.js
@@ -26,21 +26,23 @@ Object.keys(sonofapix.head).map(function (head_element) {
     document.head.appendChild(element);
   });
 });
-
-Object.keys(sonofapix.links).map(function (each_link) {
-  var link_icon = document.createElement('i');
-  link_icon.classList = sonofapix.links[each_link].icon;
-  
-  var link_a = document.createElement('a');
-  link_a.href = sonofapix.links[each_link].href;
-  link_a.target = "_blank";
-  
-  var link_div = document.createElement('div');
-  link_div.classList = "col-xs-1";
-  console.log(link_icon, link_a, link_div);
-  
-  link_a.appendChild(link_icon);
-  link_div.appendChild(link_a);
-  
-  document.getElementById('links').appendChild(link_div);
+$(document).ready(function () {
+  Object.keys(sonofapix.links).map(function (each_link) {
+    var link_icon = document.createElement('i');
+    link_icon.classList = sonofapix.links[each_link].icon;
+    
+    var link_a = document.createElement('a');
+    link_a.href = sonofapix.links[each_link].href;
+    link_a.target = "_blank";
+    
+    var link_div = document.createElement('div');
+    link_div.classList = "col-xs-1";
+    console.log(link_icon, link_a, link_div);
+    
+    link_a.appendChild(link_icon);
+    link_div.appendChild(link_a);
+    
+    document.getElementById('links').appendChild(link_div);
+  });
 });
+
